3b1c7014efeb297cda807bb843d77fc09f612a0f
Kaylee Horner <kayleeahorner@gmail.com>
2020-11-10 23:51:00 -0600

    added remaining quetions

--- index.js
+++ index.js
@@ -4,54 +4,63 @@ const fs = require("fs");
 // array of questions for user
 const questions = [
     {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-          },
-          {
-            type: 'input',
-            message: 'What is the description of your project?',
-            name: 'description',
-          },
-          {
-            type: 'input',
-            message: 'What are the steps required to install your project?',
-            name: 'installation',
-          },  
-          {
-            type: 'input',
-            message: 'Please provide usage instructions for this project.',
-            name: 'usage',
-          }, 
-          {
-            type: 'list',
-            message: 'Which license applies to this project?',
-            choices: ['GNU', 'GPLv3', 'MIT', 'Unlicensed'],
-            name: 'license',
-          }, 
-          {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'github',
-          }, 
-          {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email',
-          }, 
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },  
+    {
+        type: 'input',
+        message: 'Please provide usage instructions for this project.',
+        name: 'usage',
+    }, 
+    {
+        type: 'list',
+        message: 'Which license applies to this project?',
+        choices: ['GNU', 'GPLv3', 'MIT', 'Unlicensed'],
+        name: 'license',
+    }, 
+    {
+        type: 'input',
+        message: 'Are you accepting contributions at this time?',
+        name: 'contribution',
+        default: 'No',
+    }, 
+    {
+        type: 'input',
+        message: 'How can this project be tested?',
+        name: 'test',
+    }, 
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    }, 
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    }, 
 
 ];
 
 // function to write README file
-// function writeToFile(fileName, data) {
-
+function writeToFile(fileName, data) {
    
-// fs.writeFile('README_template.md' , md , err => err ? console.error(err) : console.log("Success!")) ;
-
-
-// });
+fs.writeFile(fileName , data , err => err ? console.error(err) : console.log("Success!"));
     
-// }
+}
+
+writeToFile();
 
 // function to initialize program
 function init() {
