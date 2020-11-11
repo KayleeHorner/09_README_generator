48999d222f92b075f38a07586f27373109d1f426
Kaylee Horner <kayleeahorner@gmail.com>
2020-11-10 23:49:53 -0600

    converting template to js function for string template

---
+++ README_generator.js
@@ -0,0 +1,34 @@
+function generateTemplate(data) {
+    return `
+    # ${data.title}
+
+   $ ${data.description}
+
+    ## Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+
+
+    ## Installation
+
+
+    ## Usage
+
+
+    ## License
+    This project is licensed under the  license.
+
+
+    ## Contributing
+
+
+    ## Tests
+
+
+    ## Questions
+    If you have any questions, you can find me on Github under  or at http://github.com/. Still have questions? You can email me at: 
+`}
