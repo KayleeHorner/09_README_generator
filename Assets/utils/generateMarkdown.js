5c05a3554a220b47e8b2b2d65b8cf64b82b424ef
Kaylee Horner <kayleeahorner@gmail.com>
2020-11-11 00:08:16 -0600

    adding markdown

--- Assets/utils/generateMarkdown.js
+++ Assets/utils/generateMarkdown.js
@@ -1,8 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+    # ${data.title}
 
-`;
-}
+    $ ${data.description}
+
+    ## Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+
+    ## License
+    This project is licensed under the ${data.license} license.
+
+    ## Contributing
+    ${data.contribution}
+
+    ## Tests
+    ${data.test}
+
+    ## Questions
+    If you have any questions, you can find me on Github under at http://github.com/${data.github}. Still have questions? You can email me at: ${data.email}
+`}
 
 module.exports = generateMarkdown;
